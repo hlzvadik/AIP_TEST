bf2f0168482226cf901b2c3451d7904fd1b4b763
vadik <vadik@debian>
2025-10-25 12:36:24 +0300

    Input one size_t

--- main.cpp
+++ main.cpp
@@ -1,2 +1,15 @@
+#include <iostream>
+#include <cstddef>
+
 int main()
-{}
+{
+  size_t a = 0;
+  std::cin >> a;
+  if (std::cin.eof()){
+    std::cout << 0 << '\n';
+  }
+  else if (std::cin.fail()){
+    std::cout << "Error\n";
+    return 1;
+  }
+}
